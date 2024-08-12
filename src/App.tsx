578c542c5c7ae14a924d3ff9dcfdf8dac127ecd5
sir-rumeh <nosarumenimuentinyan@gmail.com>
2024-08-12 08:26:49 +0100

    finish initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import { Suspense } from "react";
 import LoadingIndicator from "components/common/LoadingIndicator";
-// import { ThemeProvider } from "@mui/system";
-// import { theme } from "./theme";
+import { ThemeProvider } from "@mui/system";
+import { theme } from "./theme";
 import ApplicationRoutes from "./routes";
-
-// const Routes = lazy(() => import("./routes"));
 
 function App() {
 	// const { isLoading } = useAppSelector((state: RootState) => state.loading);
 	return (
 		<Suspense fallback={<LoadingIndicator />}>
 			{/* {isLoading && <LoadingIndicator />} */}
-			{/* <ThemeProvider theme={theme}> */}
-			<ApplicationRoutes />
-			{/* </ThemeProvider> */}
+			<ThemeProvider theme={theme}>
+				<ApplicationRoutes />
+			</ThemeProvider>
 		</Suspense>
 	);
 }
--- src/components/SidebarComponent/Links.tsx
+++ src/components/SidebarComponent/Links.tsx
@@ -1,6 +1,93 @@
 /* eslint-disable */
 import { Link, useLocation } from "react-router-dom";
 import DashIcon from "assets/icons/DashIcon";
+import { useEffect, useState } from "react";
+
+const NestedLink = ({ route }: { route: RoutesType }) => {
+	const [isChildrenOpen, setIsChildrenOpen] = useState(false);
+
+	const activeRoute = (routeName: string) => {
+		return location.pathname.includes(routeName);
+	};
+	const isParentRouteActive = activeRoute(route.path);
+
+	useEffect(() => {
+		if (!location.pathname.includes(route.path)) {
+			setIsChildrenOpen(false);
+		}
+	}, [location.pathname]);
+
+	return (
+		<>
+			<Link
+				key={route.layout + route.path}
+				to={route.layout + "/" + route.path}
+				onClick={() => {
+					setIsChildrenOpen(!isChildrenOpen);
+				}}
+				className={`${isParentRouteActive && !isChildrenOpen ? "mb-3" : ""} ${
+					isParentRouteActive
+						? "relative px-2 py-4 flex hover:cursor-pointer bg-[linear-gradient(89.92deg,_#60088C_0.07%,_#A11E90_92.22%)]"
+						: "relative mb-3 px-2 py-4 flex hover:cursor-pointer"
+				}`}
+			>
+				<div className="w-full">
+					<div className="my-[3px] flex cursor-pointer items-center justify-between px-5">
+						<div className="flex items-center">
+							<span
+								className={`${
+									isParentRouteActive ? "font-bold text-white" : "font-medium text-white"
+								}`}
+							>
+								{route.icon ? route.icon : <DashIcon />}{" "}
+							</span>
+							<p
+								className={`leading-1 ml-4 flex ${
+									isParentRouteActive ? "font-bold text-white" : "font-medium text-white"
+								}`}
+							>
+								{route.name}
+							</p>
+						</div>
+						<div className="flex items-center">
+							<span
+								className={`${
+									isParentRouteActive ? "font-bold text-white" : "font-medium text-white"
+								}`}
+							>
+								<DashIcon />
+							</span>
+						</div>
+					</div>
+					{isParentRouteActive ? (
+						<div className="absolute right-0 top-px h-9 w-1 rounded-lg bg-purpleGradient" />
+					) : null}
+				</div>
+			</Link>
+			{isChildrenOpen && isParentRouteActive && (
+				<div className="p-1 mb-3">
+					<div className="pt-4 bg-purpleSecondary pb-8 px-6 mb-3 flex flex-col justify-center items-start gap-y-2">
+						{route.children?.map((childRoute: any) => {
+							const isChildRouteActive = activeRoute(childRoute.path);
+
+							return (
+								<Link
+									key={childRoute.path}
+									to={route.layout + "/" + route.path + "/" + childRoute.path}
+									className={`${
+										isChildRouteActive ? "bg-yellowPrimary text-black" : "text-white"
+									} w-full p-3 pl-6 hover:bg-yellowPrimary hover:text-black rounded-md`}
+								>
+									{childRoute.name}
+								</Link>
+							);
+						})}
+					</div>
+				</div>
+			)}
+		</>
+	);
+};
 
 export const SidebarLinks = (props: { routes: RoutesType[] }): JSX.Element => {
 	// Chakra color mode
@@ -22,83 +109,7 @@ export const SidebarLinks = (props: { routes: RoutesType[] }): JSX.Element => {
 				if (route.children && route.children.length > 0) {
 					return (
 						<>
-							<div
-								key={route.layout + route.path}
-								// to={route.layout + "/" + route.path}
-								className={`${
-									isRouteActive
-										? "relative mb px-2 py-4 flex hover:cursor-pointer bg-[linear-gradient(89.92deg,_#60088C_0.07%,_#A11E90_92.22%)]"
-										: "relative mb-3 px-2 py-4 flex hover:cursor-pointer"
-								}`}
-							>
-								<div className="w-full">
-									<div className="my-[3px] flex cursor-pointer items-center justify-between px-5">
-										<div className="flex items-center">
-											<span
-												className={`${
-													isRouteActive
-														? "font-bold text-white"
-														: "font-medium text-white"
-												}`}
-											>
-												{route.icon ? route.icon : <DashIcon />}{" "}
-											</span>
-											<p
-												className={`leading-1 ml-4 flex ${
-													isRouteActive
-														? "font-bold text-white"
-														: "font-medium text-white"
-												}`}
-											>
-												{route.name}
-											</p>
-										</div>
-										<div className="flex items-center">
-											<span
-												className={`${
-													isRouteActive
-														? "font-bold text-white"
-														: "font-medium text-white"
-												}`}
-											>
-												<DashIcon />
-											</span>
-										</div>
-									</div>
-									{isRouteActive ? (
-										<div className="absolute right-0 top-px h-9 w-1 rounded-lg bg-purpleGradient" />
-									) : null}
-								</div>
-							</div>
-							{isRouteActive && (
-								<div className="p-1 mb-3">
-									<div className="pt-4 bg-purpleSecondary pb-8 px-6 mb-3 flex flex-col justify-center items-start gap-y-2">
-										{route.children?.map((childRoute) => {
-											const isChildRouteActive = activeRoute(childRoute.path);
-
-											return (
-												<Link
-													key={childRoute.path}
-													to={
-														route.layout +
-														"/" +
-														route.path +
-														"/" +
-														childRoute.path
-													}
-													className={`${
-														isChildRouteActive
-															? "bg-yellowPrimary text-black"
-															: "text-white"
-													} w-full p-3 pl-6 hover:bg-yellowPrimary hover:text-black rounded-md`}
-												>
-													{childRoute.name}
-												</Link>
-											);
-										})}
-									</div>
-								</div>
-							)}
+							<NestedLink route={route} />
 						</>
 					);
 				} else {
