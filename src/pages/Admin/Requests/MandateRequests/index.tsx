afb51126d337288bd0a9b88fe7b167acc5d6d805
sir-rumeh <nosarumenimuentinyan@gmail.com>
2024-09-26 08:04:23 +0100

    mandate requests index table integration

--- src/pages/Admin/Requests/MandateRequests/index.tsx
+++ src/pages/Admin/Requests/MandateRequests/index.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { mandateRequestsList } from 'utils/constants';
 import TableFilter from 'components/TableFilter';
-import { TabsProps } from 'utils/interfaces';
+import { QueryParams, TabsProps } from 'utils/interfaces';
 import CustomTabs from 'hoc/CustomTabs';
 import { TabsListTabNames } from 'utils/enums';
 import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
@@ -16,12 +16,18 @@ import {
 import { RequestTypes } from 'utils/enums';
 import CustomTable from 'components/CustomTable';
 import { useFormik } from 'formik';
-import { useMediaQuery } from '@mui/material';
+import { Box, CircularProgress, useMediaQuery } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+import { getMandateRequests } from 'config/actions/dashboard-actions';
 
 const MandateRequests = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const total = 20;
   const [activeTab, setActiveTab] = useState(TabsListTabNames.Pending);
+  const [paginationData, setPaginationData] = useState({
+    pageNumber: 1,
+    pageSize: 10,
+  });
   const tabsList: TabsProps[] = [
     {
       tabIndex: 1,
@@ -52,15 +58,20 @@ const MandateRequests = () => {
     },
   });
 
+  const [queryParams, setQueryParams] = useState<QueryParams>({
+    mandateCode: '',
+    status: activeTab,
+    pageNo: paginationData.pageNumber,
+    pageSize: paginationData.pageSize,
+    sortBy: 'asc',
+    sortOrder: 'desc',
+  });
+
+  useEffect(() => {
+    setQueryParams((prev) => ({ ...prev, status: activeTab }));
+  }, [activeTab]);
+
   const columns: GridColDef[] = [
-    {
-      field: 'accountId',
-      headerName: 'Account ID',
-      width: screen.width < 1000 ? 200 : undefined,
-      flex: screen.width >= 1000 ? 1 : undefined,
-      headerClassName: 'ag-thead',
-      sortable: false,
-    },
     {
       field: 'merchantId',
       headerName: 'Merchant ID',
@@ -104,6 +115,8 @@ const MandateRequests = () => {
             return renderIcon(UpdateRequestIcon, 'text-lightPurple');
           case RequestTypes.Disable:
             return renderIcon(DisableRequestIcon, 'text-yellowNeutral');
+          case RequestTypes.Enable:
+            return renderIcon(CreationRequestIcon, 'text-greenPrimary');
           case RequestTypes.Deletion:
             return renderIcon(DeleteRequestIcon, 'text-redSecondary');
           default:
@@ -112,7 +125,7 @@ const MandateRequests = () => {
       },
     },
     {
-      field: 'dateRequested',
+      field: 'dateCreated',
       headerName: 'Date Requested',
       width: screen.width < 1000 ? 50 : 50,
       flex: screen.width >= 1000 ? 1 : undefined,
@@ -151,6 +164,13 @@ const MandateRequests = () => {
       },
     },
   ];
+
+  const { isLoading, data, refetch, isFetching } = useQuery({
+    queryKey: ['mandateRequests', queryParams],
+    queryFn: ({ queryKey }) => getMandateRequests(queryKey[1] as QueryParams),
+    enabled: !!queryParams.status,
+  });
+
   const isLargeWidth = useMediaQuery('(min-width:1320px)');
   return (
     <>
@@ -160,35 +180,49 @@ const MandateRequests = () => {
             <h1 className="text-lg font-semibold md:text-2xl">Mandate Requests</h1>
           </div>
         </div>
-        <div className="">
-          <div className="slide-down relative mt-5 flex flex-col items-center justify-center rounded-md bg-white p-2 md:p-4">
-            <div className="flex w-full flex-col justify-between gap-y-4 border-b pb-3 2xl:flex-row 2xl:items-center">
-              <div className="flex w-full flex-row items-center justify-start gap-6 md:gap-10 lg:w-[50%]">
-                <CustomTabs tabs={tabsList} activeTab={activeTab} setActiveTab={setActiveTab} />
-              </div>
-              <div className="slide-down flex w-full items-center lg:w-[50%] lg:justify-end">
-                <div className="">
-                  <TableFilter
-                    name={'searchMandate'}
-                    placeholder={'Search Mandate'}
-                    label={'Search Mandate'}
-                    value={searchTerm}
-                    setSearch={setSearchTerm}
-                    handleOptionsFilter={() => {}}
-                    formik={formik}
-                    fromDateName={'fromDateFilter'}
-                    toDateName={'toDateFilter'}
-                    selectName={'statusFilter'}
-                    translationX={isLargeWidth ? 350 : undefined}
-                  />
+        {isLoading || isFetching ? (
+          <div className="flex h-[30vh] flex-col items-center justify-center">
+            <Box sx={{ display: 'flex' }}>
+              <CircularProgress sx={{ color: '#5C068C' }} />
+            </Box>
+          </div>
+        ) : (
+          <div className="">
+            <div className="slide-down relative mt-5 flex flex-col items-center justify-center rounded-md bg-white p-2 md:p-4">
+              <div className="flex w-full flex-col justify-between gap-y-4 border-b pb-3 2xl:flex-row 2xl:items-center">
+                <div className="flex w-full flex-row items-center justify-start gap-6 md:gap-10 lg:w-[50%]">
+                  <CustomTabs tabs={tabsList} activeTab={activeTab} setActiveTab={setActiveTab} />
+                </div>
+                <div className="slide-down flex w-full items-center lg:w-[50%] lg:justify-end">
+                  <div className="">
+                    <TableFilter
+                      name={'searchMandate'}
+                      placeholder={'Search Mandate'}
+                      label={'Search Mandate'}
+                      value={searchTerm}
+                      setSearch={setSearchTerm}
+                      handleOptionsFilter={() => refetch()}
+                      formik={formik}
+                      fromDateName={'fromDateFilter'}
+                      toDateName={'toDateFilter'}
+                      selectName={'statusFilter'}
+                      translationX={isLargeWidth ? 350 : undefined}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="mt-6 w-full">
-              <CustomTable tableData={mandateRequestsList} columns={columns} rowCount={73} />
+              <div className="mt-6 w-full">
+                <CustomTable
+                  tableData={data?.responseData?.items}
+                  columns={columns}
+                  rowCount={data?.responseData?.totalCount}
+                  paginationData={paginationData}
+                  setPaginationData={setPaginationData}
+                />
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </section>
     </>
   );
