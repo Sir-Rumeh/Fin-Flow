09f111fcf6e104539439d6691cf820ff5e811804
OmotolaJinadu <omotola.jinadu@fcmb.com>
2024-11-06 08:31:34 +0100

    completed API implementation for refreshToken and logout

--- src/config/Axios.ts
+++ src/config/Axios.ts
@@ -1,8 +1,15 @@
 import axios, { AxiosResponse } from 'axios';
 import store from 'store/index';
 import { AppConfig } from './index';
-import { generateHeader, getUserFromLocalStorage, notifyError } from 'utils/helpers/index';
+import {
+  generateHeader,
+  getUserFromLocalStorage,
+  isAdminAuthData,
+  isMerchantAuthData,
+  notifyError,
+} from 'utils/helpers/index';
 import { uiStartLoading, uiStopLoading } from 'store/reducers/LoadingSlice';
+import { refreshMerchantToken, refreshStaffToken } from './actions/authentication-actions';
 
 const AxiosClient = axios.create({
   baseURL: AppConfig.SERVER_URL,
@@ -55,39 +62,83 @@ AxiosClient.interceptors.response.use(
   (response: AxiosResponse | any) => {
     if (!(response.status === 200)) {
       dispatch(uiStopLoading());
-      return;
+      return Promise.reject(response);
     }
     dispatch(uiStopLoading());
     return response;
   },
   async (error) => {
+    dispatch(uiStopLoading());
+
+    const originalRequest = error.config;
+
+    // Check if we are dealing with a 401 error and not already retried
+    if (error?.response?.status === 401 && !originalRequest.isRetry) {
+      originalRequest.isRetry = true;
+
+      const user = getUserFromLocalStorage();
+      if (user) {
+        try {
+          let newToken;
+
+          // Attempt to refresh the token
+          if (isAdminAuthData(user)) {
+            const res = await refreshStaffToken({
+              email: user.userData.email,
+              refreshToken: user.refreshToken,
+            });
+            console.log('res', res);
+            newToken = res?.responseData?.token;
+          } else if (isMerchantAuthData(user)) {
+            const res = await refreshMerchantToken({
+              email: user.profileData.email,
+              refreshToken: user.refreshToken,
+            });
+            console.log('res', res);
+            newToken = res?.responseData?.token;
+          }
+
+          console.log(newToken);
+
+          // If we received a new token, retry the original request
+          if (newToken) {
+            localStorage.setItem('user', JSON.stringify({ ...user, token: newToken }));
+            AxiosClient.defaults.headers.common.Authorization = `Bearer ${newToken}`;
+            originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+
+            return AxiosClient(originalRequest);
+          } else {
+            notifyError('Session expired. Please log in again.');
+            window.location.href = '/';
+            return Promise.reject(new Error('Token refresh failed. User logged out.'));
+          }
+        } catch (err) {
+          console.error('Token refresh error:', err);
+          dispatch(uiStopLoading());
+          notifyError('Session expired. Please log in again.');
+          window.location.href = '/';
+          return Promise.reject(err);
+        }
+      } else {
+        notifyError('User is not authenticated');
+        dispatch(uiStopLoading());
+        window.location.href = '/';
+      }
+    }
+
     if (error?.response?.status === 400) {
-      dispatch(uiStopLoading());
       notifyError(error?.response?.data?.responseMessage);
-      return Promise.reject(error?.response?.data?.responseMessage);
-    } else if (error?.response?.status === 401) {
-      dispatch(uiStopLoading());
-      // dispatch(logout());
-      notifyError('Your session timed out, sign in again to continue');
-      window.location.href = '/';
+      return Promise.reject(error);
     } else if (error?.response?.status === 404) {
-      const url = window.location.href.split('/').slice(0, 5).join('/');
-      dispatch(uiStopLoading());
       notifyError('Resource not found');
-      setTimeout(() => {
-        window.location.href = url;
-      }, 1500);
       return Promise.reject(error);
     } else if (error?.response?.status === 500) {
-      dispatch(uiStopLoading());
       notifyError('Something went wrong');
       return Promise.reject(error);
     } else if (error.message === networkErrorMessage) {
-      dispatch(uiStopLoading());
       notifyError(error.message);
       return Promise.reject(error);
     }
-    dispatch(uiStopLoading());
     return Promise.reject(error);
   },
 );
--- src/config/actions/authentication-actions.ts
+++ src/config/actions/authentication-actions.ts
@@ -10,9 +10,45 @@ export const loginStaff = async (payload: any | undefined) => {
   }
 };
 
+export const refreshStaffToken = async (payload: any | undefined) => {
+  try {
+    const response = await AxiosClient.post('/authenticate/user/refresh-token', payload);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const logoutStaff = async (payload: any | undefined) => {
+  try {
+    const response = await AxiosClient.post('/authenticate/user/logout', payload);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const loginMerchant = async (payload: any | undefined) => {
   try {
     const response = await AxiosClient.post('/authenticate/profile', payload);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const refreshMerchantToken = async (payload: any | undefined) => {
+  try {
+    const response = await AxiosClient.post('/authenticate/profile/refresh-token', payload);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const logoutMerchant = async (payload: any | undefined) => {
+  try {
+    const response = await AxiosClient.post('/authenticate/profile/logout', payload);
     return response.data;
   } catch (error) {
     throw error;
